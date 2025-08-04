69341899ae93ba78ac7ad44d3fcd80c20caf0efe
ZenterFlow <ZenterFlow@users.noreply.github.com>
2025-08-04 15:49:32 +0200

    Create flake.nix

---
+++ assets/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Reproducible Dev Shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            terraform
+            kubectl
+            argocd
+            docker
+          ];
+        };
+      });
+}
